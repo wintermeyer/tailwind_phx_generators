2273db91e94373f15eb06a5bdceb555faa3c4d37
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2021-01-06 11:28:27 +0100

    first commit

---
+++ lib/tailwind_phx_generators.ex
@@ -0,0 +1,18 @@
+defmodule TailwindPhxGenerators do
+  @moduledoc """
+  Documentation for `TailwindPhxGenerators`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TailwindPhxGenerators.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tailwind_phx_generators_test.exs
@@ -0,0 +1,8 @@
+defmodule TailwindPhxGeneratorsTest do
+  use ExUnit.Case
+  doctest TailwindPhxGenerators
+
+  test "greets the world" do
+    assert TailwindPhxGenerators.hello() == :world
+  end
+end
